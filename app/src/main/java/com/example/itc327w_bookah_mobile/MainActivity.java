9b9aa8a1ff44728762b7f9533f8ac74d67cf34a1
M Leluma <53694508+HarmonyTab@users.noreply.github.com>
2020-12-23 17:06:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/itc327w_bookah_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.itc327w_bookah_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

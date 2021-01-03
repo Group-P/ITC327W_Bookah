20a75c800ef3fc2121f9251284a24d949760435b
M Leluma <53694508+HarmonyTab@users.noreply.github.com>
2021-01-03 09:53:42 +0200

    210034408 - 03 Jan 2021 Sun - Update - I updated the Home.xml layout's constraints. I added code on the Home.java activity to display the user's name on the Dashboard.

--- app/src/main/java/com/example/itc327w_bookah_mobile/Activities/Home.java
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Activities/Home.java
@@ -3,14 +3,25 @@ package com.example.itc327w_bookah_mobile.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.itc327w_bookah_mobile.Common.Common;
 import com.example.itc327w_bookah_mobile.R;
 
 public class Home extends AppCompatActivity {
+
+    TextView textUsername;
+    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tvHello = (TextView) findViewById(R.id.tvHello);
+        tvHello.setText(Common.currentUser.getFirstName());
+
+        textUsername = (TextView) findViewById(R.id.textUsername);
+        textUsername.setText(Common.currentUser.getLastName());
     }
 }

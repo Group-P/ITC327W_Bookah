d7c257215790001785461fa5790c2f11e181ff14
Diteboho Lethoko <66359155+Diteboho@users.noreply.github.com>
2021-01-11 14:47:18 +0200

    216005388 - 11 Jan 2021 Mon - Implementing - Implemented storage, internet and camera permissions

--- app/src/main/java/com/example/itc327w_bookah_mobile/Activities/Home.java
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Activities/Home.java
@@ -23,7 +23,6 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
 
-//<<<<<<< Updated upstream
     TextView textFirstName;
     TextView textLastName;
     LinearLayout layoutLogout;
--- app/src/main/java/com/example/itc327w_bookah_mobile/Activities/Search.java
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Activities/Search.java
@@ -1,9 +1,13 @@
 package com.example.itc327w_bookah_mobile.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.itc327w_bookah_mobile.AccountFragment;
 import com.example.itc327w_bookah_mobile.AppUtilities.SectionsPagerAdapter;
@@ -30,7 +34,7 @@ public class Search extends AppCompatActivity {
         mTabLayout = findViewById(R.id.tabs);
         mViewPager = findViewById(R.id.viewpager_container);
 
-        setupViewPager();
+        verifyPermissions();
     }
 
     private void setupViewPager(){
@@ -47,5 +51,21 @@ public class Search extends AppCompatActivity {
         mTabLayout.getTabAt(1).setText(getString(R.string.fragment_watch_list));
         mTabLayout.getTabAt(2).setText(getString(R.string.fragment_post));
         mTabLayout.getTabAt(3).setText(getString(R.string.fragment_account));
+    }
+
+    private void verifyPermissions() {
+        Log.d(TAG, "verifyPermissions: asking user for permissions");
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[2]) == PackageManager.PERMISSION_GRANTED) {
+            setupViewPager();
+        }
     }
 }

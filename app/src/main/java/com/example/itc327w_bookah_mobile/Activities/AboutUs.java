ea0bf90634335bc5f94080f3c7d53e857854bd61
Diteboho Lethoko <66359155+Diteboho@users.noreply.github.com>
2021-01-13 19:10:10 +0200

    216005388 - 13 Jan 2021 Wed - Added - Added new Activities to the project

---
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Activities/AboutUs.java
@@ -0,0 +1,16 @@
+package com.example.itc327w_bookah_mobile.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itc327w_bookah_mobile.R;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
---
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Activities/RequestedBooks.java
@@ -0,0 +1,16 @@
+package com.example.itc327w_bookah_mobile.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itc327w_bookah_mobile.R;
+
+public class RequestedBooks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requested_books);
+    }
+}

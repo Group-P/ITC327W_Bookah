2412b0794b86a15641293efd58ff10814fdd7d07
Diteboho Lethoko <66359155+diteboho@users.noreply.github.com>
2021-01-21 14:43:36 +0200

    216005388 - 21 Jan 2021 Thu - Add - Added the ItemClickListener.java interface for click functionality in the recycler view

---
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.itc327w_bookah_mobile.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}

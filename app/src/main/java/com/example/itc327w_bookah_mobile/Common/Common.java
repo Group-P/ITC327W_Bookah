17bb6bdff5bec3df2dc3c1a6e69e1348b0530f64
Diteboho Lethoko <66359155+diteboho@users.noreply.github.com>
2021-01-26 02:52:34 +0200

    216005388 - 26 Jan 2021 Tue - Commented Out - Commented out the code used to check user's internet activity as it was slowing down the debugging process

--- app/src/main/java/com/example/itc327w_bookah_mobile/Common/Common.java
+++ app/src/main/java/com/example/itc327w_bookah_mobile/Common/Common.java
@@ -14,7 +14,7 @@ public class Common {
     public static final String USER_KEY = "User";
     public static final String PWD_KEY = "Password";
 
-    public static boolean isConnectedToInternet(Context context) {
+   /* public static boolean isConnectedToInternet(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityManager != null) {
@@ -27,5 +27,5 @@ public class Common {
             }
         }
         return false;
-    }
+    }*/
 }
